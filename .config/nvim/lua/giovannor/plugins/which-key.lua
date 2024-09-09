2f18f0251061c4e2f93355892537aecd811b07a6
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2024-09-09 09:29:06 +0700

    update nvim which key

--- .config/nvim/lua/giovannor/plugins/which-key.lua
+++ .config/nvim/lua/giovannor/plugins/which-key.lua
@@ -1,13 +1,9 @@
 return {
-  "folke/which-key.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  init = function()
-    vim.o.timeout = false
-    -- vim.o.timeoutlen = 0
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
 }
