70841836d4202a4bce5ba0da08783da9b3e0b47d
Giovanno Rachmat <giovannorachmat@gmail.com>
2024-09-13 18:21:51 +0700

    update options.lua

--- .config/nvim/lua/giovannor/core/options.lua
+++ .config/nvim/lua/giovannor/core/options.lua
@@ -9,13 +9,13 @@ opt.guicursor = ""
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
-opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 4       -- 4 spaces for tabs
-opt.softtabstop = 4   -- 4 spaces for soft tab stops
-opt.shiftwidth = 4    -- 4 spaces for indent width
-opt.expandtab = true  -- expand tab to spaces
+opt.tabstop = 4 -- 4 spaces for tabs
+opt.softtabstop = 4 -- 4 spaces for soft tab stops
+opt.shiftwidth = 4 -- 4 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 opt.smartindent = true
 
@@ -24,7 +24,7 @@ opt.wrap = false -- disable line wrapping
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
@@ -35,7 +35,7 @@ opt.cursorline = true -- highlight the current cursor line
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -46,9 +46,6 @@ opt.clipboard = "unnamedplus" -- use system clipboard as default register
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
-
--- turn off swapfile
-opt.swapfile = false
 
 -- highlighting
 opt.hlsearch = false
@@ -61,7 +58,7 @@ opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
 opt.undolevels = 10000
 
--- left 8 lines above and below when scrolling
+-- left 12 lines above and below when scrolling
 opt.scrolloff = 12
 opt.signcolumn = "yes"
 
@@ -70,4 +67,4 @@ opt.colorcolumn = "150"
 
 opt.confirm = true
 
-vim.bo.commentstring = '-- %s'
+vim.bo.commentstring = "-- %s"
