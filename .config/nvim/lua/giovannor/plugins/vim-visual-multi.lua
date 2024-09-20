10e3629ed01eb23bcfc0e4d6e15b8b606540bc33
Giovanno Rachmat <giovannorachmat@gmail.com>
2024-09-20 21:04:01 +0700

    add new keymap for multi cursor up & down

--- .config/nvim/lua/giovannor/plugins/vim-visual-multi.lua
+++ .config/nvim/lua/giovannor/plugins/vim-visual-multi.lua
@@ -2,4 +2,10 @@ return {
     "mg979/vim-visual-multi",
     branch = "master",
     event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        vim.g.VM_maps = {
+            ["Add Cursor Up"] = "<C-S-Up>",
+            ["Add Cursor Down"] = "<C-S-Down>"
+        }
+    end
 }
