dca97b7d36f41bf2e9f614f59413e300da9977fd
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2024-10-06 22:03:46 +0700

    update vim visual multi, disable lazy

--- .config/nvim/lua/giovannor/plugins/vim-visual-multi.lua
+++ .config/nvim/lua/giovannor/plugins/vim-visual-multi.lua
@@ -1,11 +1,11 @@
 return {
-	"mg979/vim-visual-multi",
-	branch = "master",
-	lazy = true,
-	config = function()
-		vim.g.VM_maps = {
-			["Add Cursor Up"] = "<C-S-Up>",
-			["Add Cursor Down"] = "<C-S-Down>",
-		}
-	end,
+  "mg979/vim-visual-multi",
+  branch = "master",
+  lazy = false,
+  config = function()
+    vim.g.VM_maps = {
+      ["Add Cursor Up"] = "<C-S-Up>",
+      ["Add Cursor Down"] = "<C-S-Down>",
+    }
+  end,
 }
