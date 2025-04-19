98bc1725337deb0dada53285926c8fa8435d87a9
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-04-19 17:20:07 +0700

    updated lazyvim package

--- nvim-LazyVim/lua/plugins/colorscheme.lua
+++ nvim-LazyVim/lua/plugins/colorscheme.lua
@@ -1,14 +1,36 @@
 return {
   {
-    "eldritch-theme/eldritch.nvim",
+    "neanias/everforest-nvim",
+    version = false,
     lazy = false,
-    priority = 1000,
-    opts = {},
+    priority = 1000, -- make sure to load this before all the other start plugins
+    name = "everforest",
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      --   require("everforest").setup({
+      --     -- Your config here
+      --   })
+      vim.cmd.colorscheme("everforest")
+    end,
   },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "eldritch",
-    },
-  },
+  -- {
+  --   "eldritch-theme/eldritch.nvim",
+  --   -- lazy = false,
+  --   -- priority = 100,
+  --   name = "eldritch",
+  --   config = function()
+  --     require("eldritch").setup({
+  --       transparent = true,
+  --       terminal_colors = true,
+  --     })
+  --
+  --     -- vim.cmd.colorscheme("eldritch")
+  --   end,
+  -- },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "everforest",
+  --   },
+  -- },
 }
