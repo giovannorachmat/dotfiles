8dff9a6763789207e7c9bd2e3232e31699706f8c
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-07-14 10:17:27 +0700

    updated exports and kitty config

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,8 +7,7 @@ export BREW_PATH="/home/linuxbrew/.linuxbrew"
 eval "$($BREW_PATH/bin/brew shellenv)"
 
 # PATH configuration
-export ROFI_SCRIPTS_PATH=".config/rofi/scripts"
-export PATH="$HOME/bin:$HOME/.local/bin:$HOME/$ROFI_SCRIPTS_PATH:$BREW_PATH/bin:/usr/local/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.local/bin:$HOME/$BREW_PATH/bin:/usr/local/bin:$PATH"
 
 # XDG and general environment
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
@@ -16,14 +15,6 @@ export TERM=xterm-256color
 export LANG=en_US.UTF-8
 export WORDCHARS='*?_-.[]~&;!#$%^(){}<>'
 
-# Application settings
 export EDITOR=nvim
 export VISUAL=nvim
 export BAT_THEME="Monokai Extended Bright"
-
-# GCP settings
-export GCP_ZONE="asia-southeast2-a"
-
-# EDITOR
-export EDITOR=nvim
-export VISUAL=nvim
