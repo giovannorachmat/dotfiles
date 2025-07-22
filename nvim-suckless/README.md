9982734ddf7b29d2b8f58a541bddbed6c824757b
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-07-22 16:24:08 +0700

    added new nvim config

---
+++ nvim-suckless/README.md
@@ -0,0 +1,4 @@
+# nvim-suckless
+
+Taken from `https://github.com/radleylewis/nvim-lite/tree/youtube_demo`.
+Credits to Radley E. Sidwell-lewis.
---
+++ nvim-suckless/lua/config/theme.lua
@@ -0,0 +1,17 @@
+-- remove borders when in nvim
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+	callback = function()
+		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+		if not normal.bg then
+			return
+		end
+		io.write(string.format("\027Ptmux;\027\027]11;#%06x\007\027\\", normal.bg))
+		io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+	end,
+})
+
+-- theme & transparency
+vim.cmd.colorscheme("unokai")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
