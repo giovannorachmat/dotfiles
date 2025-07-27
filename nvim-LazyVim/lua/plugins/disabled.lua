4c7a2face5a4e89ab9b9e1b6827ac31dece01ebc
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-07-27 21:18:58 +0700

    deleted nvim-lazyvim

--- nvim-LazyVim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "snacks.nvim", opts = { scroll = { enabled = false } } },
-  { "flash.nvim", enabled = false },
-  { "bufferline.nvim", enabled = false },
-  -- { "tokyonight.nvim", enabled = false },
-}
