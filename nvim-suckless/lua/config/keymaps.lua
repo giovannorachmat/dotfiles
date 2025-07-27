04bb90fa591e1e4691208a854a0c2d62a7ee1a25
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-07-27 21:19:31 +0700

    updated suckless config to use native lsp

--- nvim-suckless/lua/config/keymaps.lua
+++ nvim-suckless/lua/config/keymaps.lua
@@ -46,6 +46,7 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
 -- Quick file navigation
 vim.keymap.set("n", "<leader>e", ":Explore<CR>", { desc = "Open file explorer" })
 vim.keymap.set("n", "<leader>ff", ":find ", { desc = "Find file" })
+vim.keymap.set("n", "<leader>ef", ":Ex", { desc = "Open File Tree" })
 
 -- Better J behavior
 vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
