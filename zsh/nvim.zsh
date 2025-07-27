9961bcb933b6be1fc3bd9e08b87dce3d1e542a7f
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-07-27 21:19:36 +0700

    updated nvims function

--- zsh/nvim.zsh
+++ zsh/nvim.zsh
@@ -8,7 +8,7 @@ alias svi="sudo vim"
 # Taken from Elijah Manor
 
 function nvims() {
-  items=("default" "LazyVim" "suckless")
+  items=("default" "minimal" "suckless")
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
     echo "Nothing selected"
