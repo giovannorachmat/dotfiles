8aab6ad9f1774257fa410ccf4fee47397f9e2355
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-09-19 19:28:07 +0700

    updated oil keybinds and enable snacks explorer

--- nvim-minimal/lua/plugins/oil.lua
+++ nvim-minimal/lua/plugins/oil.lua
@@ -1,23 +1,22 @@
 return {
-    "stevearc/oil.nvim",
-    lazy = false,
-    dependencies = { "echasnovski/mini.icons" },
-    config = function()
-        local oil = require("oil")
-        oil.setup({
-            default_file_explorer = false,
-            delete_to_trash = true,
-            skip_confirm_for_simple_edits = true,
-            view_options = {
-                show_hidden = true,
-                natural_order = true,
-            },
-            win_options = {
-                wrap = true
-            },
-            cleanup_delay_ms = 500
-        })
+	"stevearc/oil.nvim",
+	dependencies = { "echasnovski/mini.icons" },
+	config = function()
+		local oil = require("oil")
+		oil.setup({
+			default_file_explorer = false,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				show_hidden = true,
+				natural_order = true,
+			},
+			win_options = {
+				wrap = true,
+			},
+			cleanup_delay_ms = 500,
+		})
 
-        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    end,
+		vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
 }
--- nvim-minimal/lua/plugins/snacks.lua
+++ nvim-minimal/lua/plugins/snacks.lua
@@ -1,20 +1,39 @@
 return {
-    "folke/snacks.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    lazy = true,
-    ---@type snacks.Config
-    opts = {
-        bufdelete = { enabled = true },
-        lazygit = {
-            enabled = true,
-            configure = true,
-        },
-    },
-    keys = {
-        { "<leader>lg", function() Snacks.lazygit.open() end, desc = "Open Lazygit (snacks)" },
-        { "<leader>bd", function() Snacks.bufdelete() end,    desc = "Delete buffer (snacks)" },
-    }
-
+	"folke/snacks.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	lazy = true,
+	---@type snacks.Config
+	opts = {
+		explorer = { enabled = true },
+		bufdelete = { enabled = true },
+		lazygit = {
+			enabled = true,
+			configure = true,
+		},
+	},
+	keys = {
+		{
+			"<leader>lg",
+			function()
+				Snacks.lazygit.open()
+			end,
+			desc = "Open Lazygit (snacks)",
+		},
+		{
+			"<leader>bd",
+			function()
+				Snacks.bufdelete()
+			end,
+			desc = "Delete buffer (snacks)",
+		},
+		{
+			"-",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "Explorer (snacks)",
+		},
+	},
 }
