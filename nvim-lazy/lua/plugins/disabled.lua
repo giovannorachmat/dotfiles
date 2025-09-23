e851a6fa108540add1b102dee1391135c050e272
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-09-23 11:33:32 +0700

    created new folder for native lazyvim

---
+++ nvim-lazy/lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  { "bufferline.nvim", enabled = false },
+  { "lualine.nvim", enabled = false },
+  { "flash.nvim", enabled = false },
+  { "mini.indentscope", enabled = false },
+  { "folke/snacks.nvim", opts = {
+    scroll = { enabled = false },
+    picker = { enabled = false },
+  } },
+}
---
+++ nvim-lazy/lua/plugins/tpope.lua
@@ -0,0 +1,7 @@
+return {
+    { 'tpope/vim-fugitive' },
+    { 'tpope/vim-markdown' },
+    { 'tpope/vim-repeat' },
+    { 'tpope/vim-surround' },
+    { 'tpope/vim-unimpaired' }
+}
