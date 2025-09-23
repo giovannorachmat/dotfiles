6d185eebf440a5ea3f53326aed45693bd0a34901
OY! Giovanno Rachmat <giovanno.rachmat@oyindonesia.com>
2025-09-23 15:17:48 +0700

    updated nvim treesitter for nvim

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,27 +1,27 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            ensure_installed = {
-                "dockerfile",
-                "sql",
-                "json",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "vim",
-                "vimdoc",
-            },
-            auto_install = false,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false
-            },
-            indent = { enable = true },
-
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"dockerfile",
+				"sql",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"vim",
+				"vimdoc",
+			},
+			auto_install = false,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+		})
+	end,
 }
