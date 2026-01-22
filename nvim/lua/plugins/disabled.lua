eb18bb8c473147014a9524006347046824c62453
grittyindomitable <giovannorachmat@gmail.com>
2026-01-22 16:10:00 +0700

    restore lua dir in nvim/

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  { "bufferline.nvim", enabled = false },
+  { "lualine.nvim", enabled = false },
+  { "flash.nvim", enabled = false },
+  { "mini.indentscope", enabled = false },
+  { "folke/snacks.nvim", opts = {
+    scroll = { enabled = false },
+  } },
+}
